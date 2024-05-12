99c10c8369bfb9590a8ce0d17ee90d21b25249ab
Joshua Beed <70536517+jbeed33@users.noreply.github.com>
2024-05-11 21:39:04 -0700

    navbar hamburger menu implemented

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,41 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 
 import "./Navbar.css";
 function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  function toggleMenu() {
+    console.log("toggle menu called");
+    setOpenMenu((prevOpenMenu) => !prevOpenMenu);
+  }
+
   return (
-    <nav>
-      <div id="nav-wrapper">
-        <CiMenuBurger id="nav-hamburger" />
-        <ul id="nav-container">
-          <li>
-            <a href="#" className="nav-link">
-              About Me
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Experience
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav>
+        <div
+          id="nav-wrapper"
+          className={`${openMenu ? "hamburger-active" : ""}`}
+        >
+          <CiMenuBurger
+            id="nav-hamburger"
+            className={`${openMenu ? "hamburger-active" : ""}`}
+            onClick={toggleMenu}
+          />
+          <ul
+            id="nav-container"
+            className={`${openMenu ? "hamburger-active" : ""}`}
+          >
+            <li
+              id="close-button"
+              className={`${openMenu ? "hamburger-active" : ""}`}
+              onClick={toggleMenu}
+            >
+              X
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                About Me
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
