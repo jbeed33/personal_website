1f2ff6ba1789144f6d8667c296928ff949f5140c
Joshua Beed <70536517+jbeed33@users.noreply.github.com>
2024-05-20 14:34:46 -0700

    footer is responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,9 +9,23 @@ function Footer() {
           <h1>Get In Touch :)</h1>
           <p>What to learn more about me & What I create?</p>
           <div id="footer-icons-container">
-            <div className="footer-icon">Linkedin</div>
-            <div className="footer-icon">Github</div>
-            <div className="footer-icon">Email</div>
+            <a
+              className="footer-icon"
+              target="_blank"
+              href="http://www.linkedin.com/in/joshua-beed"
+            >
+              <p>Linkedin</p>
+            </a>
+            <a target="_blank" href="https://github.com/jbeed33">
+              <p>Github</p>
+            </a>
+            <a
+              className="footer-icon"
+              target="_blank"
+              href="mailto: jbeed33@gmail.com"
+            >
+              <p>Email</p>
+            </a>
           </div>
         </div>
       </section>
